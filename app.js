0403184a176bad81ead1a2643d0ee4c61b9978f8
Sam Agnew <sagnew@eden.rutgers.edu>
2014-04-06 03:58:10 -0400

    Probably just did nothing

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.use(express.errorHandler());
+
 app.get('/', function(req, res){
     res.redirect("https://api.venmo.com/v1/oauth/authorize?client_id=1675&scope=make_payments%20access_profile");
 });
 
 app.get('/authorized', function(req, res){
     accessToken = req.query.access_token;
-    res.send("Access token: " + accessToken);
+    res.send(accessToken);
 });
 
 app.listen(1337);
