5feb804d2a807b7a90497655c958f98805ceb917
faiq <faiqrazarizvi@gmail.com>
2014-04-06 02:57:18 -0400

    "render 2048 on endpoint"

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    app = express();
+app.use(express.static(path.join(__dirname, '2048')));
 
 app.get('/', function(req, res){
     res.redirect("https://api.venmo.com/v1/oauth/authorize?client_id=1675&scope=make_payments%20access_profile");
@@ -9,6 +12,9 @@ app.get('/authorized', function(req, res){
     accessToken = req.query.access_token;
     res.send("Access token: " + accessToken);
 });
-
+app.get('/2048', function(req, res){
+    console.log('end point reached');
+    res.sendfile(__dirname + "/2048/index.html");
+});
 app.listen(1337);
 console.log('Listening on port 1337');
