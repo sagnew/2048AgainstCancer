1d875d10e69351d5129d31907f186052e3cb5991
faiq <faiqrazarizvi@gmail.com>
2014-04-06 03:47:14 -0400

    ian did some shit

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express = require('express'),
     http = require('http'),
     path = require('path'),
     app = express();
-app.use(express.static(path.join(__dirname, '2048')));
+app.use(express.logger('dev'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('2048', express.static('2048'));
+// app.use(express.static(path.join(__dirname, '2048')));
 app.get('/', function(req, res){
+    console.log('req to home');
     res.redirect("https://api.venmo.com/v1/oauth/authorize?client_id=1675&scope=make_payments%20access_profile");
 });
 
@@ -12,9 +16,9 @@ app.get('/authorized', function(req, res){
     accessToken = req.query.access_token;
     res.send("Access token: " + accessToken);
 });
-app.get('/2048', function(req, res){
+app.get('/game', function(req, res){
     console.log('end point reached');
-    res.sendfile(__dirname + "/2048/index.html");
+    res.sendfile(__dirname + "/views/index.html");
 });
 app.listen(1337);
 console.log('Listening on port 1337');
