8560dfa44cd3bb4d0f69ed27f314a8936b0f5caa
Sam Agnew <sagnew@eden.rutgers.edu>
2014-04-06 02:36:08 -0400

    Update endpoints

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ app.get('/', function(req, res){
     res.redirect("https://api.venmo.com/v1/oauth/authorize?client_id=1675&scope=make_payments%20access_profile");
 });
 
-app.get('/authenticated', function(req, res){
-    console.log(req.body);
-    res.send("hello");
+app.get('/authorized', function(req, res){
+    accessToken = req.query.access_token;
+    res.send("Access token: " + accessToken);
 });
 
 app.listen(1337);
